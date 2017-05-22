5807309096411907d51e2529b28c4bd2ae2564e4
xiexuanzheng123 <349328984@qq.com>
2017-05-22 22:55:27 +0800

    module, resolve, plugin, external

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 var webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     devtool: 'cheap-module-eval-source-map', //设置调试代码的方式
@@ -10,4 +12,29 @@ module.exports = {
         path:'./build',
         filename:'bundle.js'
     },
-}
+    module: {
+        preLoader: [
+            {test: /\.js$/,loader: "eslint-loader", exclude: /node_modules/}
+        ],
+        loaders: [
+            {test: /\.less$/, loader: 'style-loader!css-loader!less-loader'},
+            {test: /\.(png|jpg)/, loader:'url-loder?limit=8192'}
+        ]
+    },
+    resolve: {
+        alias: {
+            'react-dom': path.join(__dirname, 'node_modules/react-dom/dist/react-dom.min')
+        },
+        extensions: ['', '.js', '.jsx', '.json', '.css']
+    },
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.NoErrorsPlugin(),
+        new ExtractTextPlugin('[name]-[hash:5].min.css')
+    ],
+    externals: {
+        jQuery: true
+    }
+};
