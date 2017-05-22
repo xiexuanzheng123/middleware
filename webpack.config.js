a58a71eefd3426d46e2682b1fa2755036b04256c
xiexuanzheng123 <349328984@qq.com>
2017-05-22 22:24:06 +0800

    entry and output

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,12 @@ var webpack = require('webpack');
 
 module.exports = {
     devtool: 'cheap-module-eval-source-map', //设置调试代码的方式
-    entry: './main.js'
+    entry: [
+        'webpack/hot/only-dev-server',
+        './main.js'
+    ],
+    output: {
+        path:'./build',
+        filename:'bundle.js'
+    },
 }
