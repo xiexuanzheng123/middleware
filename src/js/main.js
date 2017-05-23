d0887c08df1645b051eaedacfcc13587e1a1f370
xiexuanzheng123 <349328984@qq.com>
2017-05-23 18:05:16 +0800

    the usage of html-webpack-plugin

---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+import '../css/style.css';
+
+console.log('This is just a test !!');
+document.getElementById('header').style.backgroundColor = 'red';
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,25 @@
 var webpack = require('webpack');
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 module.exports = {
     devtool: 'cheap-module-eval-source-map', //设置调试代码的方式
-    entry: [
-        'webpack/hot/only-dev-server',
-        './main.js'
-    ],
+    entry: {
+        hmr: [
+            // 'eventsource-polyfill', // necessary for hot reloading with IE
+            'webpack-hot-middleware/client?reload=true'    // note that it reload the page it hot module reload    
+        ],
+        main: [
+            './src/js/main.js'
+        ],
+        test: [
+            './src/js/test.js'
+        ]
+    },
     output: {
-        path: path.join(__dirname,'./build'),
-        filename:'bundle.js',
+        path: path.join(__dirname,'/build'),
+        filename:'[name]bundle.js',
         publicPath: '/'
     },
     module: {
@@ -18,9 +27,12 @@ module.exports = {
         //     {test: /\.js$/,loader: "eslint-loader", exclude: /node_modules/}
         // ],
         loaders: [
-            {test: /\.less$/, loader: 'style-loader!css-loader!less-loader'},
+            {
+                test: /\.css$/, 
+                loader: 'css-loader'
+            },
             {test: /\.(png|jpg)/, loader:'url-loder?limit=8192'},
-            {test: /\.js$/, loaders: ['react-hot', 'babel'],include: path.join(__dirname,'src')}
+            {test: /\.js$/, loader: 'babel-loader',include: path.join(__dirname,'src')}
         ]
     },
     resolve: {
@@ -32,9 +44,22 @@ module.exports = {
     plugins: [
         new webpack.optimize.OccurrenceOrderPlugin(),
         new webpack.HotModuleReplacementPlugin(),
-        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            // compress: {
+            //     warning: false
+            // }
+        }),
         new webpack.NoErrorsPlugin(),
-        //new ExtractTextPlugin('[name]-[hash:5].min.css')
+        new HtmlWebpackPlugin({
+            title:'myapp',
+            filename:'build/admin.html',
+            template:'index.html',
+            inject: 'body',
+            favicon: './src/images/green.jpg',
+            hash: true,
+            chunks: ['hmr', 'test']
+        }),
+        //new ExtractTextPlugin('src/css/[name].css')
     ],
     externals: {
         jQuery: true
